dae5674d0c489e13b77c09e6a351a538b74ea39c
rehanahmed92 <rehanahmad.92@gmail.com>
2020-06-16 00:44:13 +0500

    commit complete

---
+++ app.js
@@ -0,0 +1,89 @@
+// // // Chapter - 1 (Alerts)
+
+// // // Task-1:
+// alert ("Welcome to our website");
+
+// // // Task-2
+// alert ("Error! Please enter a valid password.");
+
+// // // Task-3
+// alert ("Welcome to JS Land...\nHappy Coding!");
+
+// // // Task-4
+// alert ("Welcome to JS Land...");
+// alert ("Happy Coding!\nPrevent this page for creating any aadditional dialoges");
+
+// // // Task-5
+// console.log ("Hello... I can run JS through my web browser's console");
+
+// // Task-6
+// // Use of script tag in HTML file.
+
+// /////////////////////////////////////////////////////////////////////////////////////////////
+
+// // // Chapter - 2 (Variables for Strings)
+
+// // Task-1
+// var username
+
+// Task-2
+// var myName = "RehanAhmed";
+
+// Task-3
+// var message = "Hello World";
+// alert (message);
+
+// Task-4
+// var name = "Rehan Ahmed";
+// alert ( "Student's Name: " + name );
+// var age = "27 years";
+// alert ( "Student's Age: " + age );
+// var course = "Web & Mobile Hybrid Application Development Course (Saylani)";
+// alert ( "Student's Course: " + course );
+
+// Task-5
+// var dish = "PIZZA";
+
+// Task-6
+// var email = "rehanahmad.92@gmail.com";
+// alert ( "My Email is " + email );
+
+// Task-7
+// var book = "A smarter way to learn JavaScrit";
+// alert ( "I am trying to learn from the book " + book );
+
+// Task-8
+// document.write ( "Yah! I can write HTML content through JavaScript");
+
+// Task-9
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// Chapter - 3 (Variables for Numbers)
+
+// Task-1
+// var age = (27);
+// alert ( "I am " + age + " years old" );
+
+// Task-2
+// var vists = (14);
+// alert ( "You have visited this site " + vists + " times");
+
+// Task-3
+// var birthYear = (1992);
+// document.write ("My birth year is 1992");
+// document.write ( "<br>");
+// document.write ("Data type of my declared variable is number");
+
+// Task-4
+// var buyer = ( "Irfan Ahmed" );
+// var product = ( "T-shirts" );
+// var quantity = ( 5 );
+// document.write ( buyer + " ordered " + quantity + " " + product +  " on XYZ Clothing store." );
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Chapter - 4 (Variable Names Legal and Illegal)
+
+// Task-1
+
