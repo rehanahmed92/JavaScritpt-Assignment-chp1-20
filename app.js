e943a2eea220203b05cb2935ec7630fee269df43
Rehan Ahmed <64492984+rehanahmed92@users.noreply.github.com>
2020-06-16 02:14:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
 // // // Chapter - 1 (Alerts)
 
 // // // Task-1:
-// alert ("Welcome to our website");
+alert ("Welcome to our website");
 
 // // // Task-2
-// alert ("Error! Please enter a valid password.");
+alert ("Error! Please enter a valid password.");
 
 // // // Task-3
-// alert ("Welcome to JS Land...\nHappy Coding!");
+alert ("Welcome to JS Land...\nHappy Coding!");
 
 // // // Task-4
-// alert ("Welcome to JS Land...");
-// alert ("Happy Coding!\nPrevent this page for creating any aadditional dialoges");
+alert ("Welcome to JS Land...");
+alert ("Happy Coding!\nPrevent this page for creating any aadditional dialoges");
 
 // // // Task-5
-// console.log ("Hello... I can run JS through my web browser's console");
+console.log ("Hello... I can run JS through my web browser's console");
 
 // // Task-6
-// // Use of script tag in HTML file.
+// Use of script tag in HTML file.
 
 // /////////////////////////////////////////////////////////////////////////////////////////////
 
 // // // Chapter - 2 (Variables for Strings)
 
 // // Task-1
-// var username
+var username
 
 // Task-2
-// var myName = "RehanAhmed";
+var myName = "RehanAhmed";
 
 // Task-3
-// var message = "Hello World";
-// alert (message);
+var message = "Hello World";
+alert (message);
 
 // Task-4
-// var name = "Rehan Ahmed";
-// alert ( "Student's Name: " + name );
-// var age = "27 years";
-// alert ( "Student's Age: " + age );
-// var course = "Web & Mobile Hybrid Application Development Course (Saylani)";
-// alert ( "Student's Course: " + course );
+var name = "Rehan Ahmed";
+alert ( "Student's Name: " + name );
+var age = "27 years";
+alert ( "Student's Age: " + age );
+var course = "Web & Mobile Hybrid Application Development Course (Saylani)";
+alert ( "Student's Course: " + course );
 
 // Task-5
-// var dish = "PIZZA";
+var dish = "PIZZA";
 
 // Task-6
-// var email = "rehanahmad.92@gmail.com";
-// alert ( "My Email is " + email );
+var email = "rehanahmad.92@gmail.com";
+alert ( "My Email is " + email );
 
 // Task-7
-// var book = "A smarter way to learn JavaScrit";
-// alert ( "I am trying to learn from the book " + book );
+var book = "A smarter way to learn JavaScrit";
+alert ( "I am trying to learn from the book " + book );
 
 // Task-8
-// document.write ( "Yah! I can write HTML content through JavaScript");
+document.write ( "Yah! I can write HTML content through JavaScript");
 
 // Task-9
 
@@ -62,28 +62,63 @@
 // Chapter - 3 (Variables for Numbers)
 
 // Task-1
-// var age = (27);
-// alert ( "I am " + age + " years old" );
+var age = (27);
+alert ( "I am " + age + " years old" );
 
 // Task-2
-// var vists = (14);
-// alert ( "You have visited this site " + vists + " times");
+var vists = (14);
+alert ( "You have visited this site " + vists + " times");
 
 // Task-3
-// var birthYear = (1992);
-// document.write ("My birth year is 1992");
-// document.write ( "<br>");
-// document.write ("Data type of my declared variable is number");
+var birthYear = (1992);
+document.write ("My birth year is 1992");
+document.write ( "<br>");
+document.write ("Data type of my declared variable is number");
 
 // Task-4
-// var buyer = ( "Irfan Ahmed" );
-// var product = ( "T-shirts" );
-// var quantity = ( 5 );
-// document.write ( buyer + " ordered " + quantity + " " + product +  " on XYZ Clothing store." );
+var buyer = ( "Irfan Ahmed" );
+var product = ( "T-shirts" );
+var quantity = ( 5 );
+document.write ( buyer + " ordered " + quantity + " " + product +  " on XYZ Clothing store." );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Chapter - 4 (Variable Names Legal and Illegal)
 
 // Task-1
+var firstName = ( "Rehan" ),
+    middleName = ( "Ahmed" ), 
+    lastName = ( "Khan" );
 
+// Task-2
+// (5 Valid variable Names)
+var $sign = ("Dollar Sign");
+var _second = ("variable with underscore");
+var NAME = ("Rehan");
+var city = ("Karachi");
+var covid19 = ("Coronavirus family member");
+
+// (5 Invalid variable Names)
+var @first = ("first Invalid");
+var 2number = ("Second Invalid");
+var #third = ("Third Invalid");
+var .4rth = ("fourth Invalid");
+var /5fifth = ("fifth invalid");
+
+// Task-3
+// var heading = ("Rules for naming JS variables");
+document.write(heading.fontsize(6))
+document.write ( "<br>");
+document.write ( "<br>");
+document.write ( "<br>");
+document.write ("Variable names can only contain $, _, numbers, and letters. For Example: My1stsalary_200$ ");
+document.write ( "<br>");
+document.write ( "variable must begin with a $, _ and any letter. For example: $20, _name, City");
+document.write ( "<br>");
+document.write ( "Variable names are case sensitive");
+document.write ( "<br>");
+document.write ( "Variable names should not be JS Keywords");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Chapter - 5 (Math expressions)
